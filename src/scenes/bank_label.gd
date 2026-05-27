14ea512aff50745b54cd5ba1c7ab88c4565b56a7
Jack Sims <123803257+sym-z@users.noreply.github.com>
2026-05-26 17:53:04 -0700

    shader and label juice

--- src/scenes/bank_label.gd
+++ src/scenes/bank_label.gd
@@ -7,6 +7,17 @@ func _ready():
 	bank_reference.connect("set_manual", assign_label)
 func refresh_label():
 	text = "%d" % bank_reference.get_child_count()
+	bulge()
 
 func assign_label(amt : int):
 	text = "%d" % amt
+	bulge()
+
+func bulge():
+	var tween : Tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector3(2.0,2.0,2.0), 0.5)
+	tween.play()
+	tween.tween_property(self, "scale", Vector3(1.,1.,1.), 0.5)
+	tween.play()
+	await tween.finished
+	tween.kill()
