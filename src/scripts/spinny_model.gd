4c2953568181ae9cae0a4ac86625c338ca80ffcc
Jack Sims <123803257+sym-z@users.noreply.github.com>
2026-06-06 14:28:37 -0700

    spinny model w collision

--- src/scripts/spinny_model.gd
+++ src/scripts/spinny_model.gd
@@ -1,6 +1,6 @@
-extends MeshInstance3D
-var speed : float = 0.5
-
+extends AnimatableBody3D
+var speed : float = 1.
+var mouse_pos : Vector2 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +8,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	rotate(Vector3(0,1,0), speed*delta)
