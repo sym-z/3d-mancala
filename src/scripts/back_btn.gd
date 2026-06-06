06e33807e915cd7eb6f051216b1d3c42ff088314
Jack Sims <123803257+sym-z@users.noreply.github.com>
2026-06-06 16:08:07 -0700

    spinny reacts to mouse

--- src/scripts/back_btn.gd
+++ src/scripts/back_btn.gd
@@ -12,4 +12,4 @@ func _process(delta):
 	pass
 
 func hide_window():
-	visible = false
+	window.visible = false
--- src/scripts/spinny_model.gd
+++ src/scripts/spinny_model.gd
@@ -1,12 +1,22 @@
 extends AnimatableBody3D
 var speed : float = 1.
 var mouse_pos : Vector2 
+var screen_center : Vector2
+@export var center_marker : Marker2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_center = center_marker.global_position
 
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_pos = event.position
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	
+	print(screen_center)
+	#print(mouse_pos.distance_to(screen_center)*0.05)
+	speed = 1. + mouse_pos.distance_to(screen_center)*0.005
 	rotate(Vector3(0,1,0), speed*delta)
