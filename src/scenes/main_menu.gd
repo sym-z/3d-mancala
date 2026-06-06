66f7d5eb384a0fe0ced0d80ac24eede5333e25a3
Jack Sims <123803257+sym-z@users.noreply.github.com>
2026-06-06 15:49:12 -0700

    fix jitter, start on controls

--- src/scenes/main_menu.gd
+++ src/scenes/main_menu.gd
@@ -2,16 +2,19 @@ extends Control
 
 @export var play_btn : Button
 @export var settings_btn : Button
+@export var controls_btn : Button
 @export var announcer : AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 @export var appear_time : float = 2.0
 @export var fade_time : float = 2.0
 @export var model : MeshInstance3D
 @export var settings_container : MarginContainer
+@export var controls_container : MarginContainer
 func _ready():
 	modulate = Color(0,0,0,0)
 	play_btn.connect("pressed", to_game)
 	settings_btn.connect("pressed", open_settings)
+	controls_btn.connect("pressed", open_controls)
 	announcer.title()
 	var tween : Tween = get_tree().create_tween()
 	tween.tween_property(self,"modulate", Color(1,1,1,1), appear_time)
@@ -35,3 +38,6 @@ func to_game():
 func open_settings():
 	settings_container.open()
 	pass
+
+func open_controls():
+	controls_container.visible = true
--- src/scripts/back_btn.gd
+++ src/scripts/back_btn.gd
@@ -1,12 +1,15 @@
-extends AnimatableBody3D
-var speed : float = 1.
-var mouse_pos : Vector2 
+extends Button
+
+@export var window : MarginContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	connect("pressed", hide_window)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	
-	rotate(Vector3(0,1,0), speed*delta)
+func _process(delta):
+	pass
+
+func hide_window():
+	visible = false
